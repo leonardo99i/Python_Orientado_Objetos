9de8d59504486d9b57d220ebe1870054eb108217
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2022-07-14 23:47:48 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -1,7 +1,17 @@
 # It creates a class called Conta.
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
+        """
+        This function is a constructor for the class Conta. It takes four arguments: numero, titular, saldo,
+        and limite. It sets the attributes of the class to the arguments passed in.
+        
+        :param numero: account number
+        :param titular: the name of the account holder
+        :param saldo: the balance of the account
+        :param limite: the maximum amount of money that can be withdrawn from the account
+        """
         print("Objeto Criado")
+# Creating the attributes of the class.
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
