c7c377def7ed80e96c8fc02a735a80924645e1f2
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2022-09-18 14:28:46 -0300

    Criando a função extrato

--- conta.py
+++ conta.py
@@ -1,28 +1,23 @@
 # It creates a class called Conta.
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
-        """
-        This function is a constructor for the class Conta. It takes four arguments: numero, titular, saldo,
-        and limite. It sets the attributes of the class to the arguments passed in.
-        
-        :param numero: account number
-        :param titular: the name of the account holder
-        :param saldo: the balance of the account
-        :param limite: the maximum amount of money that can be withdrawn from the account
-        """
         print("Objeto Criado")
-# Creating the attributes of the class.
+        # Creating the attributes of the class.
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
-
         """
         The __init__() function is called automatically every time the class is being used to create a new
         object 
         Self é a referencia, que sabe onde encontrar o objeto
         """
 
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
 
-conta = Conta(123, "Leo", 50.5, 1000.0)
-conta_2 = Conta(321, "Nico", 100.0, 1000.0)
+
+conta = Conta(123, "Leo", 55.5, 5000.0)
+conta2 = Conta(321, "Nico", 43.9, 1000.0)
+
+conta2.extrato()
